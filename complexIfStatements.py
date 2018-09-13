0f7bc99d7d77e4b2e379e8e2bef59054bcae8161
bilmwas <bildad.mwangi@strathmore.edu>
2018-09-13 13:03:05 +0300

    finally working the way I want

--- complexIfStatements.py
+++ complexIfStatements.py
@@ -28,6 +28,10 @@
 #    print("Wait till Saturday. ")
 
 #Challenge Time!!
+country=""
+province=""
+orderTotal=0
+neworderTotal=0
 country=input("Which Country do you belong to?\n ").capitalize()
 orderTotal=float(input("What is your current order Total?\n "))
 Canada=False
@@ -38,10 +42,12 @@ if country=="Canada":
         #GST refers to General sales tax
         GST=0.05/100
         neworderTotal=(GST+100)/100*orderTotal
+        #new things happening
         #print(neworderTotal)
         print("You have been taxed General Sales Tax \non your order total and your new order total is ${0:f}".format(neworderTotal))
-      #  print("Your tax is ${0:.4f}".format(neworderTotal))
-    elif province=="Ontario"or province=="Newbrunswick" or province=="Novascotia":
+        #print("Your tax is ${0:.4f}".format(neworderTotal))
+        #This was good Bill. I have been wondering how this code is working
+    elif province == "Ontario" or province == "New"+" Brunswick".capitalize() or province == "Nova"+" Scotia".capitalize():
         #HST refers to Harmonized sales tax
         HST=0.13/100
         neworderTotal=(HST+100)/100*orderTotal
@@ -62,3 +68,35 @@ else:
 
 
 
+#country = ""
+#province = ""
+#orderTotal = 0
+#totalWithTax = 0
+#I am declaring variables to hold the tax values used in the calculations
+#That way if a tax rate changes, I only have to change it in one place instead
+#of searching through my code to see where I had a specific numeric value and updating it
+#GST = .05  
+#HST = .13
+#PST = .06
+#Ask the user what country they are from 
+#country = input("What country are you from? " )
+#if they are from Canada ask which province...don't forget they may enter Canada as CANADA, Canada, canada, CAnada
+#so convert the string to lowercase before you do the comparison
+#if country.lower() == "canada" :
+#    province = input("Which province are you from? ")
+#ask for the order total
+#orderTotal = float(input("What is your order total? "))
+#Now add the taxes
+#first check if they are from canada
+#if country.lower() == "canada" :
+#    if they are from canada, we have to change the calculation based on the province they specified
+#    if province.lower() == "alberta" :
+#        orderTotal = orderTotal + orderTotal * GST
+#    elif province.lower() == "ontario" or province.lower() == "new brunswick" or province.lower() == "nova scotia" :
+#        orderTotal = orderTotal + orderTotal * HST
+#    else :
+#        orderTotal = orderTotal + orderTotal * PST + orderTotal * GST
+#if they are not from Canada there is no tax, so the amount they entered is the total with tax
+#and no modification to orderTotal is required
+#Now display the total with taxes to the user, don't forget to format the number
+#print("Your total including taxes comes to $%.2f " % orderTotal)
